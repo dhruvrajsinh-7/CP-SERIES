8b3ee82cf176e1fc7a4adfe8c94cc2b20a5f295a
dhruvrajsinh <dhruvsss007@gmail.com>
2022-10-17 14:51:44 +0530

    daily problem added

---
+++ Daily-challenge/pengramstring.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using  namespace std;
+int main(){
+    string s;
+    cin>>s;
+    return checkIfPangram(s);
+}
+    bool checkIfPangram(string s1) {
+        set<int> s;
+        for (auto& c1: s1)
+            s.insert(c1);
+        if(s.size()==26)
+            return true;
+        return false;
+    }
