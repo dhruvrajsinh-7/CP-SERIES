1c684df4ce61a0f6b0d0e58a931227bc1928673a
dhruvrajsinh <dhruvsss007@gmail.com>
2022-10-18 11:57:54 +0530

    Array problem

---
+++ Arrays/RemoveDuplicate.cpp
@@ -0,0 +1,21 @@
+ #include<bits/stdc++.h>
+ using namespace std;
+ int main(){
+       vector<int> nums{1,2,3,1,1,3}; 
+       return removeDuplicates(nums);   
+ }
+ int removeDuplicates(vector<int>& nums) {
+        int point=1;
+        int n=nums.size();
+        if(n==0){
+            return 0;
+        }
+        for(int i=1;i<nums.size();i++){
+            if(nums[i-1]!=nums[i]){
+                nums[point]=nums[i];
+                point++;
+
+            }
+        }
+        return point;
+    }
