1a98b11393185af68f2eed2577db1341483b27bb
dhruvrajsinh <dhruvsss007@gmail.com>
2022-10-21 11:23:39 +0530

    Leetcode.219

---
+++ Hashmap/Containsduplicate2.cpp
@@ -0,0 +1,25 @@
+    #include<bits/stdc++.h>
+    using namespace std;
+    int main(){
+        int n;
+        cin>>n;
+        vector<int> a(n);
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        int k;
+        cin>>k;
+        containsNearbyDuplicate(a,k);   
+    }
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+    unordered_map<int, int> map;
+        for (int i = 0; i < nums.size(); i++) {
+            if (map.find(nums[i]) != map.end()){
+               if(abs(i - map[nums[i]]) <= k) {
+                    return true;
+               }
+            }
+            map[nums[i]] = i;
+        }
+        return false;
+    }
