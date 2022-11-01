e2bd5fa3522296dc926915088f959b06c289e059
dhruvrajsinh <dhruvsss007@gmail.com>
2022-11-01 20:56:47 +0530

    ArrayProblem Added

--- Arrays/Mergesortedarray.cpp
+++ Arrays/Mergesortedarray.cpp
@@ -1,3 +1,28 @@
+ #include<bits/stdc++.h>
+ using namespace std;
+ int main(){
+    vector<int> nums1;
+    vector<int> nums2;
+    int m,n;
+    cin>>m>>n;
+    for(int i=0;i<m;i++){
+        int x;
+        cin>>x;
+        nums1.push_back(x);
+    }
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        nums2.push_back(x);
+    }
+    merge(nums1,m,nums2,n);
+    for(int i=0;i<m+n;i++){
+         cout<<nums1[i]<<" ";
+    }
+
+    return 0;
+
+ }
  void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         int i,k;
         for(int i=0;i<m;i++){
