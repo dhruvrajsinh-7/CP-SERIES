8dabe1c1a286de7f8cbf03d9ab5698f56013c22c
dhruvrajsinh <dhruvsss007@gmail.com>
2022-11-05 21:32:35 +0530

    Binary search

---
+++ Binarysearch/Findtarget.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int arr[]={1,2,3,4,5,6,7,8,9,10};
+    int target=5;
+
+    int ans= binarysearch(arr,10,target);
+    return ans;
+}
+int binarysearch(int arr[], int n, int k) {
+        int start=0;
+        int end=n-1;
+        while(start<=end){
+          int mid = (start+end)/2;
+            if(arr[mid] == k){
+                return mid;
+                
+            }else if(arr[mid] < k){
+                start = mid+1;
+            }else{
+                end = mid-1;
+            }
+        }
+            return -1;
+    }
