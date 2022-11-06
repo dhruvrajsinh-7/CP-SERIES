24848d3cad2596e2f9e422900f494a0af6937030
dhruvrajsinh <dhruvsss007@gmail.com>
2022-11-06 10:34:43 +0530

    Daily challenge

---
+++ Daily-challenge/Orderlyqueue.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std; 
+string orderlyQueue(string s, int k) {
+        if(k==1){
+            string res=s;
+            string pos;
+            for(int i=0;i<s.size();i++){
+                pos=s.substr(i)+s.substr(0,i);
+                res=min(res,pos);
+            }
+            return res;
+        }
+        sort(s.begin(),s.end());
+        return s;
+    }
+    int main(){
+    string s;
+    cin>>s;
+    int k;
+    cin>>k;
+    string ans;
+    ans=orderlyQueue(s,k);
+    std::cout << ans << std::endl;
+}
