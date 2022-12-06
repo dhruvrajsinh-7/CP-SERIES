e2080f8fcc86f8674cda657546ce05317dc2a2d6
DHRUVRAJSINH <dhruvsss007@gmail.com>
2022-12-06 23:54:51 +0530

    Daily challenge

---
+++ Daily-challenge/Minavgdiff.cpp
@@ -0,0 +1,30 @@
+ int minimumAverageDifference(vector<int>& nums) {
+        int ans=-1;
+        long long minavg=INT_MAX;
+        // pair<long,int>>ans={INT_MAX,0};
+        int n=nums.size();
+        if(n==0){
+            return 0;
+        }
+        long long suml=0,sumr=0;
+        for(auto num:nums){
+            sumr+=num;
+        }
+        
+        for(int i=0;i<n;i++){
+            suml+=nums[i];
+            sumr-=nums[i];
+            long long tempdiff=0;
+            if(i<n-1){
+                tempdiff=abs(suml/(i+1)-sumr/(n-i-1));
+            }
+            else{
+                tempdiff=suml/(i+1);
+            }
+            if(tempdiff<minavg){
+                minavg=tempdiff;
+                ans=i;
+            }
+        }
+        return ans;
+    }
