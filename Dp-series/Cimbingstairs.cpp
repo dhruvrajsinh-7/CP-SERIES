f8278c888b2164cfc68f762b552e92421baeba9e
DHRUVRAJSINH <dhruvsss007@gmail.com>
2022-12-13 21:21:59 +0530

    changes

--- Dp-series/Cimbingstairs.cpp
+++ Dp-series/Cimbingstairs.cpp
@@ -1,15 +1,23 @@
-class Solution {
-public:
-    int climbStairs(int n) {
-        vector<int>dp(n+1,0);
-        dp[1]=1;
-        if(n==1){
-            return 1;
-        }
-        dp[2]=2;
-        for(int i=3;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
-        }
-        return dp[n];
+#include<bits/stdc++.h>
+using namespace std;
+int climbStairs(int n)
+{
+    vector<int> dp(n + 1, 0);
+    dp[1] = 1;
+    if (n == 1)
+    {
+        return 1;
     }
-};
+    dp[2] = 2;
+    for (int i = 3; i <= n; i++)
+    {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n];
+}
+int main(){
+    int n;
+    cin>>n;
+    cout<<"ans is:"<<climbStairs(n);
+    return 0;
+}
