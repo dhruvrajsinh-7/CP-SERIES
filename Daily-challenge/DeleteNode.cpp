496ded454ae6e4a8a188ce705a1cb3878b029e8c
DHRUVRAJSINH <dhruvsss007@gmail.com>
2022-12-31 16:30:14 +0530

    linkedlist

--- Daily-challenge/DeleteNode.cpp
+++ Daily-challenge/DeleteNode.cpp
@@ -7,16 +7,28 @@ struct ListNode {
       ListNode(int x) : val(x), next(NULL) {}
 };
 //we have used simple algo in which we just store the data of node into the temporary variable and then change the pointer so its basically doesn't removed from memory 
-int main(){
-    ListNode* node;
-    node->val=1;
-    int head[5]={4,5,1,9};
-    deleteNode(node);
-}
+//delete node in linkedlist
+//void deletenode(ListNode* node){
+//    ListNode* temp=node->next;
+//    node->val=temp->val;
+//    node->next=temp->next;
+//    delete temp;
+//}
 void deleteNode(ListNode* node) {
-        ListNode* temp=node->next;
-        node->val=temp->val;
-        node->next=temp->next;
-        temp->next=nullptr;
-        delete(temp);
+     ListNode* temp=node->next;
+     node->next=node->next->next;
+     delete(temp);
+}
+
+int main(){
+    ListNode* head=new ListNode(4);
+    head->next=new ListNode(5);
+    head->next->next=new ListNode(1);
+    head->next->next->next=new ListNode(9);
+    deleteNode(head);
+    while(head!=NULL){
+        cout<<head->val<<" ";
+        head=head->next;
+    }
+    return 0;
 }
--- LinkedList/middeoflist.cpp
+++ LinkedList/middeoflist.cpp
@@ -1,3 +1,5 @@
+#include <bits/stdc++.h>
+using namespace std;
 struct ListNode
 {
     int val;
@@ -16,4 +18,15 @@ ListNode *middleNode(ListNode *head)
         fast = fast->next->next;
     }
     return slow;
+}
+int main()
+{
+    ListNode *head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+    ListNode *mid = middleNode(head);
+    cout << mid->val << endl;
+    return 0;
 }
