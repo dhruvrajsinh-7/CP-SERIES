b0313b8869509be3faaeae3ad63c0f2e2a6d6eef
DHRUVRAJSINH <dhruvsss007@gmail.com>
2023-01-07 14:44:14 +0530

    Daily problem

---
+++ Daily-challenge/Icecreambar.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int maxIceCream(vector<int> &costs, int coins)
+{
+    sort(costs.begin(), costs.end());
+    int c = -1, sum = 0;
+    int i = 0;
+    for (auto it : costs)
+    {
+        if (it <= coins)
+        {
+            coins -= it;
+            c++;
+        }
+        else
+        {
+            return i;
+        }
+        i++;
+    }
+    return costs.size();
+}
+int main()
+{
+    vector<int> costs = {1, 3, 2, 4, 1};
+    int coins = 7;
+    cout << maxIceCream(costs, coins);
+    return 0;
+}
