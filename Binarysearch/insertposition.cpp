a3bd814df4fdf213ab403fe2b5f35d971eb0f0e5
DHRUVRAJSINH <dhruvsss007@gmail.com>
2023-02-20 13:29:56 +0530

    binary search

--- Binarysearch/insertposition.cpp
+++ Binarysearch/insertposition.cpp
@@ -1,36 +1,39 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    vector<int>nums;
+int main()
+{
+    vector<int> nums;
     int target;
-    cin>>target;
+    cin >> target;
     int n;
-    cin>>n;
-    for(int i=0;i<n;i++){
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
         int x;
-        cin>>x;
+        cin >> x;
         nums.push_back(x);
     }
-    int ans= searchInsert(nums,target);
+    int ans = searchInsert(nums, target);
     return ans;
 }
-int searchInsert(vector<int>& nums, int target) {
-       int s = 0 , e = nums.size() - 1 ,ans = -1;
-        while(s<=e)
-        {
-        int mid =(s+e)/2;
-        if(nums[mid]==target)
+int searchInsert(vector<int> &nums, int target)
+{
+    int high = nums.size() - 1, low = 0;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (target == nums[mid])
         {
             return mid;
         }
-        else if(target>nums[mid])
-        {   s=mid+1;
-            ans=mid+1;
+        else if (target > nums[mid])
+        {
+            low = mid + 1;
         }
         else
-        {    ans=mid;
-            e=mid-1;
+        {
+            high = mid - 1;
         }
-        }
-        return ans;
     }
+    return low;
+}
